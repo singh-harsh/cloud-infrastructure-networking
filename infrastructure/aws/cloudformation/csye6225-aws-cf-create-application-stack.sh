a3a8ace56362ed68fc79938668f2eb624d370736
Harsh Singh <singh.har@husky.neu.edu>
2018-10-13 15:21:34 -0400

    CloudFormation application stack added

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -12,7 +12,7 @@ if [[ $CreatedStackList = *"$networkStackname"* ]]; then
 fi
 read -p "Enter application stack name: " applicationStackName
 echo "Creating application stack '$applicationStackName' using cloudformation template csye6225-cf-application.json..."
-aws cloudformation create-stack --stack-name $applicationStackName --template-body file://csye6225-cf-application.json
+aws cloudformation create-stack --stack-name $applicationStackName --capabilities CAPABILITY_IAM --template-body file://csye6225-cf-application.json
 aws cloudformation wait stack-create-complete --stack-name $applicationStackName
 CreatedStackList=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --query "StackSummaries[*].StackName" --output text)
 if [[ $CreatedStackList = *"$applicationStackName"* ]]; then
