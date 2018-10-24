7ea4750655575a95f278e876056237f328280b35
Hardik Jain <hardikj@localhost.localdomain>
2018-10-24 18:40:58 -0400

    Tomcat dependency woohoo!!

--- webapp/cloud-application/src/main/java/com/neu/CloudApplicationMain.java
+++ webapp/cloud-application/src/main/java/com/neu/CloudApplicationMain.java
@@ -4,11 +4,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class CloudApplicationMain {
+public class CloudApplicationMain extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudApplicationMain.class, args);
