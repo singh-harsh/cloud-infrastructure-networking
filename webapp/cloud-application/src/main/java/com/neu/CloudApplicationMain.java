80bdd809f4e2ee08bf09e89867213e85df68844b
Hardik Jain <hardikj@localhost.localdomain>
2018-10-24 19:25:20 -0400

    Properties woohoo!!

--- webapp/cloud-application/src/main/java/com/neu/CloudApplicationMain.java
+++ webapp/cloud-application/src/main/java/com/neu/CloudApplicationMain.java
@@ -2,6 +2,8 @@ package com.neu;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,7 +13,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class CloudApplicationMain extends SpringBootServletInitializer {
 
+    public static final Log LOGGER = LogFactory.getLog(CloudApplicationMain.class);
+
+    @Value("${spring.datasource.driver-class-name}")
+    private static String className;
     public static void main(String[] args) {
+        LOGGER.info(className);
         SpringApplication.run(CloudApplicationMain.class, args);
     }
 }
