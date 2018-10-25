9ae302a73390dd7e271892d9bea4afe1928b29fe
Hardik Jain <hardikj@localhost.localdomain>
2018-10-25 02:06:36 -0400

    AWS Config added woohoo!!

--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-cicd-stack.sh
@@ -3,6 +3,9 @@ read choice
 while [ "$choice" != "y" ] || [ "$choice" != "n" ]; do  
 if [ "$choice" = "y" ]; then
 read -p "Enter cicd stack name: " stackname
+webApiS3Bucket=$(aws cloudformation describe-stack-resource --stack-name $applicationStackName --logical-resource-id "S3BucketCodeDeploy" --query StackResourceDetail.PhysicalResourceId --output text)
+echo "Emptying the S3 bucket..."
+aws s3 rm s3://$webApiS3Bucket --recursive
 echo "Deleting stack '$stackname'..."
 aws cloudformation delete-stack --stack-name $stackname
 aws cloudformation wait stack-delete-complete --stack-name $stackname
