7984442bd0c1785351475f0bbbc20fad240d608d
Harsh Singh <singh.har@husky.neu.edu>
2018-11-12 23:37:41 -0500

    deployment changes and cloudwatch added to ec2

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -2,3 +2,5 @@
 
 sudo systemctl stop tomcat.service
 sudo systemctl start tomcat.service
+sudo systemctl start cloudWatch.service
+sudo systemctl enable cloudWatch.service
--- infrastructure/aws/codedeploy/applicationStop.sh
+++ infrastructure/aws/codedeploy/applicationStop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo systemctl stop tomcat.service
+sudo systemctl stop cloudWatch.service
