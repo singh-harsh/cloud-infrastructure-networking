571c76caed399745eb53a447543d54970f16bdbf
Harsh Singh <singh.har@husky.neu.edu>
2018-11-15 02:03:24 -0500

    wohooo poilicy changes

--- webapp/cloud-application/src/main/java/com/neu/metrics/RequestMetricsLogging.java
+++ webapp/cloud-application/src/main/java/com/neu/metrics/RequestMetricsLogging.java
@@ -3,8 +3,6 @@ package com.neu.metrics;
 import com.timgroup.statsd.NoOpStatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,13 +19,8 @@ public class RequestMetricsLogging {
     @Value("${metrics.server.port}")
     private int metricsServerPort;
 
-    private static final Log LOGGER = LogFactory.getLog(RequestMetricsLogging.class);
-
     @Bean
     public StatsDClient metricsClient() {
-        LOGGER.info(publishMetrics);
-        LOGGER.info(metricsServerHost);
-        LOGGER.info(metricsServerPort);
         if (true) {
             return new NonBlockingStatsDClient("csye6225", "localhost", 8125);
         }
