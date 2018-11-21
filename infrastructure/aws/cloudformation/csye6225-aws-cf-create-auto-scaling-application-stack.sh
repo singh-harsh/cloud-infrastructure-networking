5fba5060ef965538b4037f089455f408009a5166
Harsh Singh <singh.har@husky.neu.edu>
2018-11-21 14:36:56 -0500

    deployment group moved to autoscaling stack

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -1,15 +1,16 @@
 #APPLICATION STACK CREATION
-read -p "Enter application stack name: " applicationStackName
+read -p "Enter auto scaling application stack name: " applicationStackName
 DNSRecordName=$(aws route53 list-hosted-zones-by-name --query HostedZones[0].Name --output text)
 domainName=${DNSRecordName%?}
+Ec2TagValue=csye6225-ec2
 HostedZoneResourceId=$(aws route53 list-hosted-zones --query 'HostedZones[0].Id' --output text | cut -d "/" -f 3)
 CertificateArnNumber=$(aws acm list-certificates --query CertificateSummaryList[0].CertificateArn --output text)
-echo "Creating application stack '$applicationStackName' using cloudformation template csye6225-cf-application.json..."
-aws cloudformation create-stack --stack-name $applicationStackName --capabilities CAPABILITY_IAM --template-body file://csye6225-cf-application.json --parameters ParameterKey=domainName,ParameterValue=$domainName ParameterKey=HostedZoneResourceId,ParameterValue=$HostedZoneResourceId ParameterKey=CertificateArnNumber,ParameterValue=$CertificateArnNumber ParameterKey=DNSRecordName,ParameterValue=$DNSRecordName
+echo "Creating auto scaling application stack '$applicationStackName' using cloudformation template csye6225-cf-application.json..."
+aws cloudformation create-stack --stack-name $applicationStackName --capabilities CAPABILITY_IAM --template-body file://csye6225-cf-auto-scaling-application.json --parameters ParameterKey=domainName,ParameterValue=$domainName ParameterKey=HostedZoneResourceId,ParameterValue=$HostedZoneResourceId ParameterKey=CertificateArnNumber,ParameterValue=$CertificateArnNumber ParameterKey=DNSRecordName,ParameterValue=$DNSRecordName ParameterKey=Ec2TagValue,ParameterValue=$Ec2TagValue ParameterKey=StackName,ParameterValue=$applicationStackName
 aws cloudformation wait stack-create-complete --stack-name $applicationStackName
 CreatedStackList=$(aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE --query "StackSummaries[*].StackName" --output text)
 if [[ $CreatedStackList = *"$applicationStackName"* ]]; then
-    echo "Application Stack $applicationStackName created successfully"
+    echo "Auto scaling application Stack $applicationStackName created successfully"
     else
-    echo "Application Stack $applicationStackName creation failed"
+    echo "Auto scaling application Stack $applicationStackName creation failed"
 fi
